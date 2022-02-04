8b59c5955900dd2775a9ba56cc5757881e5c71a7
sventec <sventec@users.noreply.github.com>
2022-02-04 10:14:40 -0500

    Initial commit of weeks 2 and 3 scripts

---
+++ week02/portscanner.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -o errexit # Exit on most errors
+
+# Assign input arguments to variables
+hostfile=$1
+portfile=$2
+
+# Assign socket connection error file
+errfile="/tmp/portscan_err"
+
+# Verify the input arguments
+if [ ! -f $hostfile ]; then
+  echo "Error: hostfile does not exist"
+  exit 1
+fi
+
+if [ ! -f $portfile ]; then
+  echo "Error: portfile does not exist"
+  exit 1
+fi
+
+echo "host,port"
+
+for host in $(cat $hostfile); do
+  for port in $(cat $portfile); do
+    timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>errfile && echo "$host,$port"
+  done
+done
+
+# Check for presence of stderr
+if [ -s errfile ]; then
+  echo "Presence of errors from connections!"
+  cat errfile
+  rm errfile
+  exit 1
+fi
+
---
+++ week03/portscanner2.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# USAGE:
+# ./portscanner2.sh <IP PREFIX> <PORT>
+# E.G.: ./portscanner2.sh 10.0.5 53
+
+set -o errexit # Exit on most errors
+
+# Assign input arguments to variables
+prefix=$1
+port=$2
+
+# Assign socket connection error file
+errfile="/tmp/portscan_err"
+
+echo "ip,port"
+
+for host in $1.{1..254}; do
+  timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>errfile && echo "$host,$port"
+done
+
+# Check for presence of stderr
+if [ -s errfile ]; then
+  echo "Presence of errors from connections!"
+  cat errfile
+  rm errfile
+  exit 1
+fi
+
